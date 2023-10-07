98ba3fd1f21803cf299dfa308d21992d6af1d0a0
RbtC137 <128915939+RbtC137@users.noreply.github.com>
2023-10-07 21:35:08 +0800

    Complete basic UI and js code

--- script/wooden-fish.js
+++ script/wooden-fish.js
@@ -1,3 +1,19 @@
 const woodenFish = document.querySelector('.js-wooden-fish');
+var merits = 0;
+const msgBox = document.querySelector('.msg-box');
+woodenFish.addEventListener('click',()=>{
+    merits++;
+    // msgBox.classList.toggle('merits-accumulate-animation');
+    // setTimeout(()=>{msgBox.classList.remove('merits-accumulate-animation');},500);
+    startAnimate();
+})
 
-woodenFish.addEventListener('click',()=>{alert()})
+function startAnimate() {
+    const div = document.createElement("div");
+    div.classList.add("merits-accumulate-animation");
+    div.innerText = "功德 + 1";
+    msgBox.appendChild(div);
+    setTimeout(() => {
+      div.remove();
+    }, 100000);
+  }
